760427dfc5552ab4c2322869a6f1850f338c97aa
agusNasi <agustinnasi11@gmail.com>
2023-06-26 10:23:49 -0300

    changes to server.js

--- src/app.js
+++ src/app.js
@@ -63,11 +63,7 @@ app.set('views', path.resolve(__dirname, './views'));
 app.set('view engine', 'handlebars');
 
 //Server
-const server = app.listen(PORT, '127.0.0.1', () => {
-  const host = server.address().address;
-  const port = server.address().port;
-  logGreen(`Server is up and running on http://${host}:${port}`);
-});
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // Server error
 server.on('error', (error) => {
